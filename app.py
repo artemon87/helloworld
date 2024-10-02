e9b98005b3a7455b426ba6a05443facc2554a6ce
Artem K. <kan.artemon@gmail.com>
2024-10-02 10:10:40 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify({"message": "Hello, World Updated!"})
+    return jsonify({"message": "Hello, World Back to Old!"})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
